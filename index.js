6fd6763ce1f86638154add6606461fa7cc66e8f8
Heta Kaneriya <164718883+heta-kaneriya@users.noreply.github.com>
2024-03-25 12:36:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const imgBox = document.querySelector("#imgBox");
+const qrText = document.querySelector("#qrText");
+const qrImage = document.querySelector("#qrImage");
+
+function generateQR() {
+  if (qrText.value.length > 0) {
+    qrImage.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      qrText.value;
+    imgBox.classList.add("show-img");
+  }else{
+    qrText.classList.add("error");
+    setTimeout(() =>{
+      qrText.classList.remove("error");
+    },1000);
+  }
+}
